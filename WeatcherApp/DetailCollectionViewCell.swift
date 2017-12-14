2bf0f0a3fe3f97f8982700cf3de5cec44107fd7d
BlackSkyNight <matejczyk.mateusz@gmail.com>
2017-12-14 14:47:47 +0100

    UITableView with UICollectionView in UITableViewCell

---
+++ WeatcherApp/DetailCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  DetailCollectionViewCell.swift
+//  WeatcherApp
+//
+//  Created by Mateusz Matejczyk on 14.12.2017.
+//  Copyright © 2017 Mateusz Matejczyk. All rights reserved.
+//
+
+import UIKit
+
+class DetailCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var iconImage: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+}
---
+++ WeatcherApp/WeatherTableViewCell.swift
@@ -0,0 +1,33 @@
+//
+//  WeatherTableViewCell.swift
+//  WeatcherApp
+//
+//  Created by Mateusz Matejczyk on 14.12.2017.
+//  Copyright © 2017 Mateusz Matejczyk. All rights reserved.
+//
+
+import UIKit
+
+class WeatherTableViewCell: UITableViewCell {
+
+    // init elements
+    
+    @IBOutlet weak var namePlaceLabel: UILabel!
+    @IBOutlet weak var nameDayLabel: UILabel!
+    @IBOutlet weak var iconImage: UIImageView!
+    @IBOutlet weak var temperatureLabel: UILabel!
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
